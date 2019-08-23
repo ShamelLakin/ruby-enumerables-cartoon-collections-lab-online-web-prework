a84318624e46d56c2a64c5ba1fb0466b1be65a58
Shamel Lakin <christlife4u@gmail.com>
2019-08-23 16:00:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,11 @@ def roll_call_dwarves(dwarfs)
    planeteer_calls.map {|name| name.capitalize + "!" }
  end
 
-# def long_planeteer_calls# code an argument here
-#   # Your code here
-# end
+ def long_planeteer_calls# code an argument here
+   # Your code here
+ end
 
-# def find_the_cheese# code an argument here
-#   # the array below is here to help
-#   cheese_types = ["cheddar", "gouda", "camembert"]
-# end
+ def find_the_cheese# code an argument here
+   # the array below is here to help
+   cheese_types = ["cheddar", "gouda", "camembert"]
+ end
