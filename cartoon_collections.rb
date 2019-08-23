2506adf6e88f928530df9733f27cb3e9f50a339b
Shamel Lakin <christlife4u@gmail.com>
2019-08-23 16:39:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ calls_long = ["axe", "earth", "wind", "fire"]
   end
  end
 
- def find_the_cheese# code an argument here
+ def find_the_cheese(cheese_types)
    # the array below is here to help
    cheese_types = ["cheddar", "gouda", "camembert"]
  end
