2ce7b01235d712a75333691c1b763c827d05f844
Shamel Lakin <christlife4u@gmail.com>
2019-08-23 15:47:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,9 +3,8 @@ dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(dwarfs)
   dwarfs.each_with_index { |item, index| puts "#{index + 1}:#{item}" }
   end
-
- def summon_captain_planet [planeteer_calls]
-   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+ planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+ def summon_captain_planet (planeteer_calls)
    planeteer_calls.map! {|name| name.capitalize + "!" }
  end
 
