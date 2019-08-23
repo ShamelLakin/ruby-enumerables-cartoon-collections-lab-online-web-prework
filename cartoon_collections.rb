c0d91729b219028caf1ef0e73ea994277db5001f
Shamel Lakin <christlife4u@gmail.com>
2019-08-23 15:43:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 require "pry"
 dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
 def roll_call_dwarves(dwarfs)
-  dwarfs.each_with_index { |item, index| puts "#{index}:#{item}" }
+  dwarfs.each_with_index { |item, index| puts "#{index + 1}:#{item}" }
   end
 
 # def summon_captain_planet [planeteer_calls]
