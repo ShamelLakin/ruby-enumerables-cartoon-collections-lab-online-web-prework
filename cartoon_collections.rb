38c50c9f066a19b6a241cb463926e5939b4313ca
Shamel Lakin <christlife4u@gmail.com>
2019-08-23 15:35:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,16 +2,16 @@ def roll_call_dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
   roll_call_dwarves.each_with_index { |item, index| puts "#{index}:#{item}" }
   end
 
-def summon_captain_planet [planeteer_calls]
-  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-  planeteer_calls.map! {|name| name.capitalize + "!" }
-end
+# def summon_captain_planet [planeteer_calls]
+#   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+#   planeteer_calls.map! {|name| name.capitalize + "!" }
+# end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls# code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
